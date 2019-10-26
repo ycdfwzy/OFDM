c27d03c22a9d9b7d41b20939b5fb88edab7730b7
ycdfwzy <ycdfwzy@outlook.com>
2019-10-27 03:23:52 +0800

    add flag code

--- OFDM.m
+++ OFDM.m
@@ -2,16 +2,25 @@ clear all;
 close all;
 clc;
 
+[~, standard_flag] = generate_sending_signal([]);
 bernoulli_binary_generator = randi([0,1], 12800, 1);
-sending_signal = generate_sending_signal(bernoulli_binary_generator);
-sending_signal = sending_signal./sum(sending_signal.^2);
+[sending_signal, ~] = generate_sending_signal(bernoulli_binary_generator);
+maxx = max(abs(sending_signal));
+minx = min(sending_signal);
 % calcFreq(sending_signal, 7000);
-% [y,Fs] = audioread('C:\Users\ycdfwzy\Desktop\20191024_161158.m4a');
-% figure(1);
-% plot(t(1:1000),y1(1:1000));
-noised_signal = awgn(sending_signal, 20, 'measured');
+noised_signal = awgn([sending_signal(end-1312:end), sending_signal, sending_signal(1: 1000)], 20, 'measured');
 % calcFreq(noised_signal, 100000);
-noised_signal = noised_signal./sum(noised_signal.^2);
-received_data = decode_received_signal(noised_signal);
-BER = sum(xor(bernoulli_binary_generator, received_data))/length(received_data);
-audiowrite('test.wav', sending_signal, 7000);
+% noised_signal = noised_signal./sum(noised_signal.^2);
+received_data = decode_received_signal(noised_signal, standard_flag);
+BER = sum(xor(bernoulli_binary_generator, received_data))/length(bernoulli_binary_generator);
+disp(BER);
+
+% test_data1 = randi([0,1], 1280, 1);
+% test_data2 = randi([0,1], 1280, 1);
+% test_data3 = [];
+% [sending_signal1, signal_complex1] = generate_sending_signal(test_data1);
+% [sending_signal2, signal_complex2] = generate_sending_signal(test_data2);
+% [sending_signal3, signal_complex3] = generate_sending_signal(test_data3);
+% noised_signal2 = [rand(1, 1000), sending_signal2]; % awgn(sending_signal2, 20, 'measured');
+% decode_received_signal(noised_signal2, signal_complex3);
+
